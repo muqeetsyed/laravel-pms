7459f1543c403c8cf9e8a055d2e910a9f60f6adf
muqeetsyed <muqeetsyed96@gmail.com>
2024-06-30 12:21:35 +0530

    Replace materail theme support with Bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,21 +1,13 @@
 <head>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-  <script type="importmap">
-    {
-      "imports": {
-        "@material/web/": "https://esm.run/@material/web/"
-      }
-    }
-  </script>
-  <script type="module">
-    import '@material/web/all.js';
-    import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
 
-    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
-  </script>
+  {{-- Bootstrap --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- Additional inline styles -->
+<<<<<<< Updated upstream
     <style>
       .primary-container {
         background-color: var(--md-sys-color-primary-container);
@@ -24,7 +16,11 @@
         border-radius: 8px;
       }
     </style>
+=======
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+>>>>>>> Stashed changes
 </head>
-<body class="container">
+<body class="container-fluid  rounded">
 
 </body>
